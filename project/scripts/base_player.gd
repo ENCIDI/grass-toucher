9e457529b216a4b0cba241676a028452b0f15492
ENCIDI <hellhuger@gmail.com>
2025-06-28 10:02:53 +0200

    player movement restructurezed

--- project/scripts/base_player.gd
+++ project/scripts/base_player.gd
@@ -3,7 +3,14 @@ extends CharacterBody2D
 var pos = position
 
 @export var speed = 800
+@export var stats = {
+	hp = 500,
+	stamina = 500
+}
 
-func _physics_process(delta: float) -> void:
+func move():
 	velocity = (Input.get_vector("move_left","move_right","move_up","move_down")*speed)
 	move_and_slide()
+
+func _physics_process(delta: float) -> void:
+	move()
